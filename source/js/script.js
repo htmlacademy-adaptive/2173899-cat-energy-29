43897cc3144c659b9674a78213d10be4f7006d79
Сергей Сакович <imperator42@rambler.ru>
2023-09-20 14:10:45 +0300

    Вносит правки по критериям

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@ let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-header__toggle');
 
 navMain.classList.remove('main-nav--nojs');
+navToggle.classList.remove('main-header__toggle--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
